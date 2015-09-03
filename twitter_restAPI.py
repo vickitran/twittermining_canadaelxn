de29797ab6ffb09fd18e572a4952dc9f387e0f1a
Victoria Tran <vickictran@gmail.com>
2015-09-03 17:39:11 -0400

    organized program into modules, new fx tweet_data and countFrequency, added main.py

--- twitter_restAPI.py
+++ twitter_restAPI.py
@@ -1,5 +1,8 @@
 import tweepy 
-import codecs # have to encode text before writing to file
+import codecs # have to encode text before writing to file 
+import re # regular expressions 
+import string 
+from collections import Counter
 
 # tonny's credentials
 access_token = "21627027-w4cJ0ugNNFIXzyt3YOj3In2YumXpk28a4K9RszLte"
@@ -12,14 +15,25 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-tweets = codecs.open("tweet.txt",'w','utf-8')
+def tweet_data(keywords):
+	f = codecs.open("tweet.txt",'w','utf-8')
+	for word in keywords:
+		public_tweets = api.search(q = word,count = 100)
+		for tweet in public_tweets:
+			# get rid of urls
+			tweet.text = re.sub('\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', tweet.text, flags=re.MULTILINE)
+			#f.write("user: " + tweet.user.screen_name + " text: " + tweet.text + " time: " + str(tweet.created_at) + ",\n" )
+			f.write(tweet.text + ",\n")
+	f.close()
 
-keywords = ['cdnpoli','elxn42']
-#public_tweets = api.user_timeline("pmharper",count = 5)
-for party in keywords:
-	public_tweets = api.search(party, rpp = 10) 
-	# rpp = The number of tweets to return per page, up to a max of 100 pages
-	for tweet in public_tweets:
-   		tweets.write(tweet.text + ",\n")
 
-tweets.close()
+# to be called after tweet_data
+def countFrequency():
+	table = string.maketrans("","")
+	f = open("tweet.txt","r")
+	c = Counter()
+	for line in f:
+		line.translate(table, string.punctuation)
+		c = Counter(line.split())
+		print c['NDP']
+
